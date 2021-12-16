638c4e5f04449f0fb10bbb43a2c769e8b46c6ec7
William Boman <william@redwill.se>
2021-12-16 04:21:48 +0100

    cmake single file support test

--- tests/cmake/lsp_spec.lua
+++ tests/cmake/lsp_spec.lua
@@ -17,9 +17,25 @@ describe("cmake", function()
 
     assert.equal(1, #buf_clients)
     assert.equal("cmake", buf_clients[1].name)
+    assert.equal(1, #buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.equal(helpers.resolve_workspace_dir("example-project-1"), buf_clients[1].config.cmd_cwd)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+
+  it("works with single file support", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-2/CMakeLists.txt")
+    vim.api.nvim_command("set ft=cmake")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("cmake", buf_clients[1].name)
     -- Single file support
     assert.is_nil(buf_clients[1].workspace_folders)
-    assert.equal(helpers.resolve_workspace_dir("example-project-1"), buf_clients[1].config.cmd_cwd)
+    assert.equal(helpers.resolve_workspace_dir("example-project-2"), buf_clients[1].config.cmd_cwd)
     assert.is_truthy(buf_clients[1].initialized)
   end)
 end)
