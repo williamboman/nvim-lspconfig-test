ad8e97e1cb77776ecab593d9280c8f3c91ad00c2
William Boman <william@redwill.se>
2021-12-16 04:13:56 +0100

    zls: add single_file_support test

--- tests/zls/lsp_spec.lua
+++ tests/zls/lsp_spec.lua
@@ -5,13 +5,7 @@ require("test.extensions")
 local it = a.tests.it
 
 describe("zls", function()
-  local workspace_dir = helpers.resolve_workspace_dir("example-project-1")
-
-  helpers.setup_server("zls", {
-    root_dir = function()
-      return workspace_dir
-    end,
-  })
+  helpers.setup_server("zls", {})
 
   it("starts", function()
     vim.api.nvim_command("bufdo bwipeout!")
@@ -19,12 +13,28 @@ describe("zls", function()
     vim.api.nvim_command("set ft=zig")
     helpers.wait_for_ready_lsp()
 
-    local buf_clients = vim.lsp.buf_get_clients()
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
 
     assert.equal(1, #buf_clients)
     assert.equal("zls", buf_clients[1].name)
     assert.equal(1, #buf_clients[1].workspace_folders)
     assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+
+  it("works with single file support", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-2/main.zig")
+    vim.api.nvim_command("set ft=zig")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("zls", buf_clients[1].name)
+    -- Single file
+    assert.is_nil(buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_dir("example-project-2"), buf_clients[1].config.cmd_cwd)
     assert.is_truthy(buf_clients[1].initialized)
   end)
 end)
