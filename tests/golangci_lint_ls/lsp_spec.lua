dabf95aa545b269046fb7b1193ecec6df717f7e6
William Boman <william@redwill.se>
2022-01-23 18:25:47 +0100

    add golangci_lint_ls

---
+++ tests/golangci_lint_ls/lsp_spec.lua
@@ -0,0 +1,24 @@
+local a = require("plenary.async")
+local helpers = require("test.helpers")
+require("test.extensions")
+
+local it = a.tests.it
+
+describe("golangci_lint_ls", function()
+  helpers.setup_server("golangci_lint_ls", {})
+
+  it("starts", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-1/main.go")
+    vim.api.nvim_command("set ft=go")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("golangci_lint_ls", buf_clients[1].name)
+    assert.equal(1, #buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+end)
