165407803f801612f41bf5bb760a26625d3dc7ca
William Boman <william@redwill.se>
2022-01-27 22:55:53 +0100

    add lelwel_ls

---
+++ tests/lelwel_ls/lsp_spec.lua
@@ -0,0 +1,27 @@
+local util = require("lspconfig.util")
+local a = require("plenary.async")
+local helpers = require("test.helpers")
+require("test.extensions")
+
+local it = a.tests.it
+
+describe("lelwel_ls", function()
+  helpers.setup_server("lelwel_ls", {
+    root_dir = util.root_pattern("root-marker"),
+  })
+
+  it("starts", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-1/src/main.llw")
+    vim.api.nvim_command("set ft=llw")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("lelwel_ls", buf_clients[1].name)
+    assert.equal(1, #buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+end)
