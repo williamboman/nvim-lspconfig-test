857ac9c7c22214740993aa37a136173aeef4180e
William Boman <william@redwill.se>
2022-01-28 19:16:39 +0100

    add zeta_note

---
+++ tests/zeta_note/lsp_spec.lua
@@ -0,0 +1,24 @@
+local a = require("plenary.async")
+local helpers = require("test.helpers")
+require("test.extensions")
+
+local it = a.tests.it
+
+describe("zeta_note", function()
+  helpers.setup_server("zeta_note", {})
+
+  it("starts", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-1/src/index.md")
+    vim.api.nvim_command("set ft=markdown")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("zeta_note", buf_clients[1].name)
+    assert.equal(1, #buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+end)
