631702bb75a0e3a8701240d7a87c849cd878bc7d
William Boman <william@redwill.se>
2022-04-01 16:54:16 +0200

    add prosemd_lsp

---
+++ tests/prosemd_lsp/lsp_spec.lua
@@ -0,0 +1,30 @@
+local a = require("plenary.async")
+local helpers = require("test.helpers")
+require("test.extensions")
+
+local it = a.tests.it
+
+describe("prosemd_lsp", function()
+  local workspace_dir = helpers.resolve_workspace_dir("example-project-1")
+
+  helpers.setup_server("prosemd_lsp", {
+    root_dir = function()
+      return workspace_dir
+    end,
+  })
+
+  it("starts", function()
+    vim.api.nvim_command("bufdo bwipeout!")
+    vim.api.nvim_command("new | only | silent edit fixtures/example-project-1/index.md")
+    vim.api.nvim_command("set ft=markdown")
+    helpers.wait_for_ready_lsp()
+
+    local buf_clients = vim.tbl_values(vim.lsp.buf_get_clients(0))
+
+    assert.equal(1, #buf_clients)
+    assert.equal("prosemd_lsp", buf_clients[1].name)
+    assert.equal(1, #buf_clients[1].workspace_folders)
+    assert.equal(helpers.resolve_workspace_uri("example-project-1"), buf_clients[1].workspace_folders[1].uri)
+    assert.is_truthy(buf_clients[1].initialized)
+  end)
+end)
